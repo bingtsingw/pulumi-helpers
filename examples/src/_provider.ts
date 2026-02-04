968ccacd80baf4c4b4cc78f523318e360f039c31
Bingqing Wang <10382462+bingtsingw@users.noreply.github.com>
2026-02-05 00:37:17 +0800

    chore: upgrade monorepo

--- examples/src/_provider.ts
+++ examples/src/_provider.ts
@@ -4,8 +4,8 @@ import * as pulumi from '@pulumi/pulumi';
 const Provider = {
   Alicloud: (region = 'cn-beijing') => {
     return new alicloud.Provider(`pulumi-helpers-alicloud-${region}`, {
-      accessKey: process.env['ALICLOUD_NANMU_AK']!,
-      secretKey: pulumi.secret(process.env['ALICLOUD_NANMU_SK']!),
+      accessKey: process.env['ALICLOUD_AK']!,
+      secretKey: pulumi.secret(process.env['ALICLOUD_SK']!),
       region,
     });
   },
