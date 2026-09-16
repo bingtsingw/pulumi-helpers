9fbc873d6f52645c83bcec20b5314cc6727760d3
Bingqing Wang <10382462+bingtsingw@users.noreply.github.com>
2026-09-16 08:47:13 +0800

    chore: 工程化升级

---
+++ components/alicloud-rds-db/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite-plus';
+
+export default defineConfig({
+  pack: {
+    entry: ['src/index.ts'],
+    format: ['cjs'],
+    outExtensions() {
+      return { js: '.js' };
+    },
+    dts: true,
+    clean: true,
+    minify: true,
+    treeshake: true,
+  },
+});
---
+++ vite.config.ts
@@ -0,0 +1,60 @@
+import { defineConfig } from 'vite-plus';
+
+export default defineConfig({
+  staged: {
+    '**/*.{js,jsx,ts,tsx,mjs,cjs,mts,cts}': 'vp check',
+    '**/*.{html,css,scss,less,md,mdx,json,jsonc,json5,yaml,yml,toml,graphql,gql,vue}': 'vp fmt',
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+    plugins: ['typescript', 'unicorn', 'oxc', 'import'],
+    rules: {
+      'no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'no-fallthrough': 'error',
+      'no-new': 'off',
+    },
+  },
+  fmt: {
+    ignorePatterns: [
+      '**/.DS_Store',
+      '**/.eslintcache',
+      '**/yarn.lock',
+      '**/pnpm-lock.yaml',
+      '**/package-lock.json',
+      '**/node_modules',
+      '**/.husky',
+      '**/*.log',
+      '**/*.ico',
+      '**/*.svg',
+      '**/*.png',
+      '**/*ignore',
+      '**/.editorconfig',
+      '**/CHANGELOG.md',
+      '**/build',
+      '**/coverage',
+      '**/dist',
+      '**/.next',
+      '**/.umi',
+      '**/.umi-test',
+      '**/.umi-production',
+    ],
+    printWidth: 120,
+    proseWrap: 'never',
+    singleQuote: true,
+    sortPackageJson: true,
+    sortImports: {
+      newlinesBetween: false,
+    },
+  },
+});
